14dbc25413a5688809ef3564c22e22927feef4f7
Enricrypto <105727501+Enricrypto@users.noreply.github.com>
2024-07-31 22:41:08 +0200

    chore: forge init

---
+++ solidity-contracts/test/LiquidityPool.t.sol
@@ -0,0 +1,6 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+import "forge-std/Test.sol";
+import {LiquidityPool} from "../src/LiquidityPool.sol";
+// import {MockERC20} from "../src/MockERC20.sol";
